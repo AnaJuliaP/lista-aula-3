9a89ede02c9d39373d5177beac112d3f6ad7e851
AnaJuliaP <100452078+AnaJuliaP@users.noreply.github.com>
2022-03-01 21:07:00 -0300

    Resolvendo Exercicio3

---
+++ lista-aula-3/src/main/java/Exercicio3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.printf("Digite uma nota: ");
+        int numero1 = entrada.nextInt();
+        System.out.printf("Digite uma nota: ");
+        int numero2 = entrada.nextInt();
+
+        
+    }
+
+
+}
