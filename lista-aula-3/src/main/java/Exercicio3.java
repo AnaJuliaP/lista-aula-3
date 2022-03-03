60aaf0797fde74dc46a41698e37241a3990f4e5a
AnaJuliaP <100452078+AnaJuliaP@users.noreply.github.com>
2022-03-02 22:29:57 -0300

    Update Exercicio3.java

--- lista-aula-3/src/main/java/Exercicio3.java
+++ lista-aula-3/src/main/java/Exercicio3.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
 public class Exercicio3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.printf("Digite uma nota: ");
-        int numero1 = entrada.nextInt();
-        System.out.printf("Digite uma nota: ");
-        int numero2 = entrada.nextInt();
+        System.out.printf("Insira notas do auluno");
+        double NP1 = entrada.nextDouble(), NP2 = entrada.nextDouble();
+        double NPA = 0,  NFA = 0;
 
-        
+        NPA = (NP1 + NP2) / 2;
+
+        if (NPA >= 60) {
+            System.out.printf("Aprovado!");
+
+        } else {
+            System.out.println("Insira nota NP3");
+            double NP3 = entrada.nextDouble();
+            NFA = (NPA + NP3) / 2;
+            if (NFA >= 50) {
+                System.out.println("Aprovado");
+            } else {
+                System.out.println("Reprovado");
+            }
+        }
+
     }
-
-
 }
